72243b171c0998c5cfc65c06a2ed27ca0f1adba6
lumenbarron <lucemendez@gmail.com>
2019-05-29 18:18:55 -0500

    adding the function to filter, data with names, ubication and images

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const data = require('./restaurant.json')
 
-console.log(data)
+console.log("restaurants", restaurants)
+
 // Searching for the type of Restaurant
-// const typePokemonName = ( nameType, data ) => {
-//     let typePokemon = data
-//       .filter( obj => obj.type[0] === nameType || obj.type[1] === nameType );
-//       console.log(JSON.stringify(typePokemon));
-//       return typePokemon;
-//   };
+const typeRestaurants = ( data, nameType ) => {
+    let typeRest = data
+    .filter( obj => obj.type[0] === nameType || obj.type[1] === nameType );
+    return typeRest
+  };
+
+//let filter;
+const select = () => {
+    document.getElementById("printType").innerHTML = '';
+    const type = document.getElementById("selectRestaurant").value;
+    console.log(type)
+    let filter = typeRestaurants(restaurants, type)
+    console.log(filter)
+    //return filter
+    filter.map(type => {
+        type;
+        const printTypes = `
+        <section>
+        <h4> ${type.title} </h4>
+        <h6> ${type.ubication}</h6>
+        <button >
+        <img class ="buttonImg" src="${type.img}">
+        </button>
+        </section>
+        `;
+        document.getElementById("printType").innerHTML += printTypes;
+    })
+    
+}
+
+document.getElementById('buttonType').addEventListener('click', select)
+
+
