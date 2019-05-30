52110e311e8947b3022ae0915a3c97fc2f7875de
lumenbarron <lucemendez@gmail.com>
2019-05-30 16:05:10 -0500

    changing the js object

--- index.js
+++ index.js
@@ -9,8 +9,34 @@ const typeRestaurants = ( data, nameType ) => {
   };
 
 //let filter;
+// const select = () => {
+//     document.getElementById("printType").innerHTML = '';
+//     const type = document.getElementById("selectRestaurant").value;
+//     console.log(type)
+//     let filter = typeRestaurants(restaurants, type)
+//     console.log(filter)
+//     //return filter
+//     filter.map(type => {
+//         type;
+//         const printTypes = `
+//         <section>
+//         <h4> ${type.title} </h4>
+//         <h6> ${type.ubication}</h6>
+//         <button >
+//         <img class ="buttonImg" src="${type.img}">
+//         </button>
+//         </section>
+//         `;
+//         document.getElementById("printType").innerHTML += printTypes;
+//     })
+    
+// }
+
+// document.getElementById('buttonType').addEventListener('click', select)
+
+//let filter;
 const select = () => {
-    document.getElementById("printType").innerHTML = '';
+    document.getElementById("galleryPrint").innerHTML = '';
     const type = document.getElementById("selectRestaurant").value;
     console.log(type)
     let filter = typeRestaurants(restaurants, type)
@@ -19,15 +45,22 @@ const select = () => {
     filter.map(type => {
         type;
         const printTypes = `
-        <section>
-        <h4> ${type.title} </h4>
-        <h6> ${type.ubication}</h6>
-        <button >
-        <img class ="buttonImg" src="${type.img}">
-        </button>
-        </section>
+        <div class="mu-single-gallery col-md-4">
+        <div class="mu-single-gallery-item">
+            <figure class="mu-single-gallery-img">
+              <a class="mu-imglink" target="_blank" href="${type.page}">
+              <img alt="img" src="${type.img}">
+               <div class="mu-single-gallery-info">
+                  <img class="mu-view-btn" src="assets/img/plus.png" alt="plus icon img">
+              </div> 
+            </a>
+            </figure>
+            <h4> ${type.title} </h4>
+            <h6> ${type.ubication}</h6>
+          </div>
+    </div>
         `;
-        document.getElementById("printType").innerHTML += printTypes;
+        document.getElementById("galleryPrint").innerHTML += printTypes;
     })
     
 }
